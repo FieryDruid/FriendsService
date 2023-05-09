8c167df9e22896da7ed36db50dd7cf45baf865c4
Pavel Krasnobaev <pavel.krasnobaev@amonitoring.ru>
2023-05-10 00:58:30 +0300

    Add views for all services

--- friendsservice/api_v1/user/views.py
+++ friendsservice/api_v1/user/views.py
@@ -11,18 +11,25 @@ from rest_framework.views import APIView
 from friendsservice.api_v1.user.handlers import ValidatePasswordHandler
 from friendsservice.api_v1.user.serializers import (
     UserDataSerializer, UserCreationErrorSerializer, UsernameSerializer, CreateFriendshipErrorSerializer,
-    SentFriendshipRequestsSerializer
+    SentFriendshipRequestsSerializer, ReceivedFriendshipRequestsSerializer, UserFriendsListSerializer
 )
 from friendsservice.friendship.exceptions import (
-    UserCannotBeFriendError, FriendshipRequestAlreadyExistsError, UserDoesNotExistsError
+    UserCannotBeFriendError, FriendshipRequestAlreadyExistsError, UserDoesNotExistsError,
+    FriendshipRequestDoesNotExistsError, HasActiveFriendship, UserNotInFriendsListError
 )
+from friendsservice.friendship.models import FriendshipStatus
 from friendsservice.friendship.services.add_to_friends import AddToFriendsService
+from friendsservice.friendship.services.change_status import ChangeFriendshipStatusService
+from friendsservice.friendship.services.check_status import CheckFriendshipStatusService
+from friendsservice.friendship.services.delete_from_friends import DeleteFromFriendsService
+from friendsservice.friendship.services.get_friends_list import GetFriendsListService
+from friendsservice.friendship.services.received_requests import GetReceivedFriendshipRequestsService
 from friendsservice.friendship.services.sent_requests import GetSentFriendshipRequestsService
 
 User = get_user_model()
 
 
-class UserView(APIView):
+class CreateUserView(APIView):
 
     @extend_schema(
         description='Create new user',
@@ -92,3 +99,107 @@ class UserSentFriendshipView(APIView):
         serialized_data = SentFriendshipRequestsSerializer(data=service_result, many=True)
         serialized_data.is_valid()
         return Response(data=serialized_data.data, status=200)
+
+
+class UserReceivedFriendshipView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description='Get received friendship requests list',
+        responses={
+            200: ReceivedFriendshipRequestsSerializer(many=True)
+        },
+        methods=['GET']
+    )
+    def get(self, request: Request) -> Response:
+        service_result = GetReceivedFriendshipRequestsService(user=request.user)()
+        serialized_data = ReceivedFriendshipRequestsSerializer(data=service_result, many=True)
+        serialized_data.is_valid()
+        return Response(data=serialized_data.data, status=200)
+
+
+class AcceptFriendshipRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description='Accept friendship request',
+        responses={
+            200: None
+        },
+        methods=['GET']
+    )
+    def get(self, request: Request, **kwargs) -> Response:
+        try:
+            ChangeFriendshipStatusService(friendship_id=kwargs['friendship_id'], status=FriendshipStatus.CONFIRMED)()
+        except FriendshipRequestDoesNotExistsError:
+            return Response({'error': 'Friendship request not found'}, status=404)
+        return Response(status=200)
+
+
+class DeclineFriendshipRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description='Decline friendship request',
+        responses={
+            200: None
+        },
+        methods=['GET']
+    )
+    def get(self, request: Request, **kwargs) -> Response:
+        friendship_id = kwargs['friendship_id']
+        try:
+            ChangeFriendshipStatusService(friendship_id=kwargs['friendship_id'], status=FriendshipStatus.DECLINED)()
+        except FriendshipRequestDoesNotExistsError:
+            return Response({'error': f'Not found active friendship request with id {friendship_id}'}, status=404)
+        return Response(status=200)
+
+
+class UserFriendshipView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description='Get friendship with user status',
+        responses={
+            200: None
+        },
+        methods=['GET']
+    )
+    def get(self, request: Request, **kwargs) -> Response:
+        target_username = kwargs['username']
+        try:
+            CheckFriendshipStatusService(target_username=target_username)()
+        except FriendshipRequestDoesNotExistsError:
+            return Response({'message': f'Not found active friendship with {target_username}'}, status=200)
+        except HasActiveFriendship as exc:
+            return Response({'message': exc.friendship_status}, status=200)
+        return Response(status=400)
+
+    @extend_schema(
+        description='Delete user from friends',
+        responses={
+            200: None,
+            404: None
+        },
+        methods=['DELETE']
+    )
+    def delete(self, request: Request, **kwargs) -> Response:
+        try:
+            DeleteFromFriendsService(sender=request.user, target_username=kwargs['username'])()
+        except (UserNotInFriendsListError, UserDoesNotExistsError):
+            return Response(status=404)
+        return Response(status=200)
+
+
+class UserFriendsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description='Get friends list',
+        responses={
+            200: UserFriendsListSerializer()
+        },
+        methods=['GET']
+    )
+    def get(self, request: Request) -> Response:
+        return Response(GetFriendsListService(user=request.user)(), status=200)
